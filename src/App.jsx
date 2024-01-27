cebcb851432ae16d839546235e533f5959bd5c9b
Makorii <marcelaromina.d@hotmail.com>
2024-01-27 13:19:22 -0300

    agrego ruteo para que el navbar cumpla su funcionabilidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import  Home  from "./components/Home"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import NewMovies from "./components/NewMovies"
+import Popular from "./components/Popular"
 
 
 function App() {
 
 
   return (
-    <></>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/new-movies" element={<NewMovies />}/>
+      <Route path="/popular" element={<Popular />}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
