796ec9af5f8d13856a39053aa96a5d39290e4e74
Makorii <marcelaromina.d@hotmail.com>
2024-01-28 14:00:12 -0300

    agrego buscador de peliculas junto con un subtitulo y ya dejo el espacio para cuando funcione el buscador y me traiga las peliculas correspondientes

--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import { Typography } from "@mui/material";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "18ch",
+      "&:focus": {
+        width: "25ch",
+      },
+    },
+  },
+}));
 
 function SearchMovies() {
   return (
-    <div>SearchMovies</div>
-  )
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar sx={{ backgroundColor: "black", justifyContent:"center", flexDirection:"column", paddingBottom:"15px" }}>
+            <Typography pb={2}>¿What do you want to see today?</Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search a movie..."
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box height="70.8vh"></Box>
+    </>
+  );
 }
 
-export default SearchMovies
+export default SearchMovies;
