d55c06558d9a5d15a7664240cc906d3bed8ca622
Makorii <marcelaromina.d@hotmail.com>
2024-01-27 21:34:53 -0300

    agrego estructura del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <Box style={{backgroundColor:"#000000", color:"white"}} p={3}>
+      <Typography variant='caption' align='center' display="block" gutterBottom>Hecho con mucho ❤ por Marcela Duran</Typography>
+    </Box>
   )
 }
 
