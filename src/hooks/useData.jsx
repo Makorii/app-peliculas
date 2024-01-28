1f8ead6bb3fc327db53701cebbd0f6634d92a4d8
Makorii <marcelaromina.d@hotmail.com>
2024-01-28 20:58:49 -0300

    modifico nombre de la carpeta

---
+++ src/hooks/useData.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+function useData() {
+    const [data, setData] = useState([])
+
+    const apiKey = import.meta.env.VITE_API_KEY_TMDB;
+
+    const moviesSlider = () => {
+      const url =
+        "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&&language=es-ES&page=1&sort_by=popularity.desc";
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+      };
+
+      fetch(url, options)
+        .then((res) => res.json())
+        .then((json) => setData(json.results.slice(0, 5)))
+        .catch((err) => console.error("error:" + err));
+    };
+    
+    
+  return {
+    data,
+    moviesSlider
+  }
+}
+
+export default useData
