f2890bc82acc49f2816ad576c8a8090048be5662
Makorii <marcelaromina.d@hotmail.com>
2024-01-30 20:14:20 -0300

    agrego funciones add, delete e isFavorite para ir probando si funciona cada una

--- src/context/FavoritesContext.jsx
+++ src/context/FavoritesContext.jsx
@@ -1,9 +1,34 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 export const FavoritesContext = createContext();
 
-const FavoritesContextProvider = () => {
+const FavoritesContextProvider = ({ children }) => {
+    const [favorites, setFavorites] = useState([])
 
+    const addFavorite = (movie) => {
+        setFavorites([...favorites, movie])
+    }
+
+    const deleteFavorite = (idMovie) => {
+        setFavorites([favorites.filter((movie) => movie.id !== idMovie)])
+    }
+
+    const isFavorite = (idMovie) => {
+        favorites.some((movie) => movie.id === idMovie)
+    }
+
+    const data = {
+        favorites,
+        addFavorite,
+        deleteFavorite,
+        isFavorite,
+    };
+
+    return (
+        <FavoritesContextProvider value={data}>
+            {children}
+        </FavoritesContextProvider>
+    ) 
 }
 
 export default FavoritesContextProvider
