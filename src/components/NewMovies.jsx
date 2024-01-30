bee553e5f1d50ea4cea305ab54707aca11b1309b
Makorii <marcelaromina.d@hotmail.com>
2024-01-30 20:13:25 -0300

    agrego corazon de favoritos

--- src/components/NewMovies.jsx
+++ src/components/NewMovies.jsx
@@ -1,8 +1,7 @@
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import IconButton from "@mui/material/IconButton";
+import { FcLikePlaceholder } from "react-icons/fc";
 import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { useEffect, useState } from "react";
@@ -21,12 +20,18 @@ function NewMovies() {
 
   return (
     <Box>
-        <Typography variant="h4" align="center" p={2}>
-          New movies
-        </Typography>
+      <Typography variant="h4" align="center" p={2}>
+        New movies
+      </Typography>
       <Box sx={{ display: "flex", flexWrap: "wrap" }} p={3}>
         {data.map((movie) => (
           <Card sx={{ width: 250, margin: "15px" }} key={movie.id}>
+            <Box display="flex" justifyContent="flex-end">
+              <FcLikePlaceholder
+                fontSize="25px"
+                style={{ position: "absolute", padding:"5px" }}
+              />
+            </Box>
             <CardMedia
               component="img"
               image={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
@@ -37,17 +42,18 @@ function NewMovies() {
                 {movie.title}
               </Typography>
             </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-            </CardActions>
           </Card>
         ))}
       </Box>
-      <Box sx={{ display: "flex", justifyContent:"center" }} p={2}>
+      <Box sx={{ display: "flex", justifyContent: "center" }} p={2}>
         <Stack spacing={2}>
-          <Pagination count={158} showFirstButton showLastButton variant="outlined" shape="rounded"/>
+          <Pagination
+            count={158}
+            showFirstButton
+            showLastButton
+            variant="outlined"
+            shape="rounded"
+          />
         </Stack>
       </Box>
     </Box>
