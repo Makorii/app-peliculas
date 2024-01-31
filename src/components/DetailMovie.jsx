40c67b76b20f468dae1060f43f797b15379a6590
Makorii <marcelaromina.d@hotmail.com>
2024-01-31 13:45:48 -0300

    armo estructura de la seccion de detalles de la pelicula seleccionada

--- src/components/DetailMovie.jsx
+++ src/components/DetailMovie.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useEffect } from 'react'
+import useData from '../hooks/useData'
+import { Box, Button, CardMedia, Typography } from '@mui/material'
 
 function DetailMovie() {
+    const { data, getMovie} = useData()
+
+    useEffect(() => {
+        getMovie(`${id}`)
+    })
+
   return (
-    <div>DetailMovie</div>
-  )
+    <Box
+      sx={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${data.poster_path})`,
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="194"
+        image={`url(https://image.tmdb.org/t/p/original${data.backdrop_path})`}
+        alt={`${data.title}`}
+      />
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4">{data.title}</Typography>
+        <Button>See trailer</Button>
+      </Box>
+      <Typography variant="subtitle1">Resume</Typography>
+      <Typography variant="body">{data.overview}</Typography>
+      <Typography variant="subtitle1">Genres</Typography>
+      {data.genres.map((genre) => (
+        <Typography variant="body">{genre}</Typography>
+      ))}
+    </Box>
+  );
 }
 
 export default DetailMovie
