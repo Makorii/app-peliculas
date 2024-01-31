c25685e01298cc06989a9df6e0026a095f9e7f7e
Makorii <marcelaromina.d@hotmail.com>
2024-01-31 15:24:00 -0300

    mejoro estructura de la seccion y agrego informacion de la api

--- src/components/DetailMovie.jsx
+++ src/components/DetailMovie.jsx
@@ -1,36 +1,53 @@
 import { useEffect } from 'react'
 import useData from '../hooks/useData'
-import { Box, Button, CardMedia, Typography } from '@mui/material'
+import { Box, Button, Card, CardMedia, Container, Typography } from '@mui/material'
+import { useParams } from 'react-router-dom';
 
 function DetailMovie() {
-    const { data, getMovie} = useData()
+    const { data, detailMovie} = useData()
+    const { id } = useParams();
 
     useEffect(() => {
-        getMovie(`${id}`)
-    })
+        detailMovie(id);
+    },[id])
 
   return (
     <Box
       sx={{
-        backgroundImage: `url(https://image.tmdb.org/t/p/original${data.poster_path})`,
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${data.backdrop_path})`,
+        height: "100vh",
+        backgroundSize: "cover",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <CardMedia
-        component="img"
-        height="194"
-        image={`url(https://image.tmdb.org/t/p/original${data.backdrop_path})`}
-        alt={`${data.title}`}
-      />
-      <Box display="flex" justifyContent="space-between">
-        <Typography variant="h4">{data.title}</Typography>
-        <Button>See trailer</Button>
+      <Box sx={{ width: "400px", padding: "25px" }}>
+        <CardMedia
+          component="img"
+          width="100%"
+          image={`https://image.tmdb.org/t/p/original${data.poster_path}`}
+          alt={`${data.title}`}
+        />
       </Box>
-      <Typography variant="subtitle1">Resume</Typography>
-      <Typography variant="body">{data.overview}</Typography>
-      <Typography variant="subtitle1">Genres</Typography>
-      {data.genres.map((genre) => (
-        <Typography variant="body">{genre}</Typography>
-      ))}
+      <Box display="flex" flexDirection="column" maxWidth="500px">
+        <Typography variant="h3">{data.title}</Typography>
+        <Typography variant="subtitle1">Resume</Typography>
+        <Typography variant="body">{data.overview}</Typography>
+        <Typography variant="subtitle1">Genres</Typography>
+        {data.genres && data.genres.length > 0 ? (
+          data.genres.map((genre) => (
+            <Typography key={genre.id} variant="body">
+              {genre.name}
+            </Typography>
+          ))
+        ) : (
+          <Typography variant="body">No genres available</Typography>
+        )}
+      </Box>
+
+      <Button>See trailer</Button>
     </Box>
   );
 }
