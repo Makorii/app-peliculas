4ffedd84b1c2c7361be99d0cbe65a9142cff30f5
Makorii <marcelaromina.d@hotmail.com>
2024-02-01 20:02:21 -0300

    arreglo responsive de las listas de home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,12 +8,21 @@ function Home() {
       <Slider />
       <Container>
         <Grid
+          xl
           container
           spacing={2}
           columns={16}
           pt={6}
           mt={6}
           columnSpacing={10}
+          sx={{
+            justifyContent: {
+              xs: "center",
+            },
+            display: {
+              xs: "flex",
+            },
+          }}
         >
           <MovieList category="popular" />
           <MovieList category="top_rated" />
